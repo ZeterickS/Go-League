b694ed3e0111ec55ca0403517596ac61c67133e1
Hack2C <tomwieczorekcb@gmail.com>
2025-01-06 17:34:24 +0100

    add command

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"flag"
 	"log"
+	"os"
+	"os/signal"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
-	"discord-bot/types"
-	"discord-bot/apiHelper"
 )
 
 
-var ( RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not") )
+var (
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally") 
+)
 
 var s *discordgo.Session
 
 func init() { flag.Parse() }
 
 func init() {
-	var err error
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -29,7 +31,7 @@ func init() {
 		log.Fatal("Bot token not found in environment variables")
 	}
 
-	s, err = discordgo.New("Bot " + *BotToken)
+	s, err = discordgo.New("Bot " + BotToken)
 
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
@@ -38,63 +40,78 @@ func init() {
 
 var (
 	commands = []*discordgo.ApplicationCommand{
-		Name: "add",
-		
+		{
+			Name: "add",
+			Description: "Add a new Summoner",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"add": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Hey there! Congratulations, you just executed your first slash command",
+				},
+			})
+		},
 	}
 )
 
-func main() {
-
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	token := os.Getenv("TOKEN")
-	if token == "" {
-		log.Fatal("Bot token not found in environment variables")
-	}
-
-	session, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Println("Error creating Discord session:", err)
-		return
-	}
-
-	session.AddHandler(messageCreate)
-
-	err = session.Open()
-	if err != nil {
-		fmt.Println("Error opening connection:", err)
-		return
-	}
-
-	fmt.Println("Bot is now running. Press CTRL+C to exit.")
-
-	stop := make(chan os.Signal, 1)
-	<-stop
-
-	session.Close()
+func init() {
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
-		return
+
+func main() {
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := s.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
 	}
 
-	if m.Content == "ping" {
-		fmt.Println("Got ping", )
-		s.ChannelMessageSend(m.ChannelID, "pong")
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer s.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+		// // We need to fetch the commands, since deleting requires the command ID.
+		// // We are doing this from the returned commands on line 375, because using
+		// // this will delete all the commands, which might not be desirable, so we
+		// // are deleting only the commands that we added.
+		// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+		// if err != nil {
+		// 	log.Fatalf("Could not fetch registered commands: %v", err)
+		// }
+
+		for _, v := range registeredCommands {
+			err := s.ApplicationCommandDelete(s.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
 
 	}
 
-	if m.Content == "summoner" {
-		fmt.Println("Got summoner", )
-		
-	    summoner1 := types.NewSummoner("Test1", "2345", "AccouintID", "IDDiesundDas", "Ich bin eine ganz bes0ondere PUUID", 1305)
-
-		message := summoner1.Rank.ToString()
-		s.ChannelMessageSend(m.ChannelID, message)
-	}
+	log.Println("Gracefully shutting down.")
 }
